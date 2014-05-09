961acfa29ed33e8020a5144f001ffad9c7bfd627
Rob Tweed <rtweed@mgateway.com>
2014-05-09 15:11:59 +0100

    Build 60.6: Cleaner handling of External Listener conflict at startup

--- startupExamples/ewdStart-gtm.js
+++ startupExamples/ewdStart-gtm.js
@@ -6,7 +6,7 @@ Notes:
 1) The configuration/startup definition below assumes that EWD.js has been installed in 
    ~/ewdjs
 
-2) IMPORTANT!: Nodem must have been installed using npm install nodem from within ~/ewdjs
+2) IMPORTANT!: Nodem must have been installed using "npm install nodem" from within ~/ewdjs
    Remember to configure Nodem properly by moving files from its repository correctly
 
 */
@@ -14,7 +14,7 @@ Notes:
 var ewd = require('ewdjs');
 
 var params = {
-      port: 8080,
+      httpPort: 8080,
       poolSize: 2,
       database: {
         type: 'gtm',
