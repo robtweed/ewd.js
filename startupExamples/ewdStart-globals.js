fb61ba3709fa5506f26e8145679cd5f07972ae7c
Rob Tweed <rtweed@mgateway.com>
2014-05-09 11:17:44 +0100

    Build 60.3 - Refinements to GlobalsDB startup file

--- startupExamples/ewdStart-globals.js
+++ startupExamples/ewdStart-globals.js
@@ -1,3 +1,19 @@
+/*
+Example EWD.js Startup file for use with GlobalsDB
+
+Notes:
+
+1) Change the database.path value as appropriate for your GlobalsDB installation
+
+2) IMPORTANT!: The cache.node interface module file MUST exist in the primary node_modules directory
+of your EWD.js configuration. 
+
+3) Comment out the cwd parameter if you want to use this startup file from the command line
+
+4) You may need to invoke this startup file as sudo if you have used Mike Clayton's Ubuntu installer for EWD.js
+
+*/
+
 var ewd = require('ewdjs');
 
 params = {
