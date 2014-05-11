9fe10815c22a18d58261a0b6ca37a4a16c849e72
Rob Tweed <rtweed@mgateway.com>
2014-05-11 23:18:52 +0100

    Build 61.4: dEWDrop configuration enhancement

--- modules/dewdrop-config.js
+++ modules/dewdrop-config.js
@@ -1,15 +1,15 @@
 var fs = require('fs');
 var exec = require('child_process').exec;
-var gtmdir = '/usr/lib/fis-gtm';
-var gtmver = fs.readdirSync(gtmdir)[0];
+var gtmdir = '/opt/lsb-gtm';
+var gtmver = '6.0-001_i686';
 
 if (!fs.existsSync('/usr/local/lib/libgtmshr.so')) {
   var contents = '#!/usr/bin/env bash\nsudo -i\ncd /usr/local/lib\nln -s ' + gtmdir + '/' + gtmver + '/libgtmshr.so\nldconfig\nexit';
   fs.writeFileSync('tmp.sh', contents, 'utf-8');
+  var ok = exec('tmp.sh', function() {
+    fs.unlinkSync('tmp.sh');
+  });
 };
-var ok = exec('tmp.sh', function() {
-  fs.unlinkSync('tmp.sh');
-});
 
 process.env['GTMCI'] = process.cwd() + '/node_modules/nodem/resources/calltab.ci';
 process.env['gtmroutines'] = process.cwd() + '/node_modules/nodem/src ' + process.env['gtmroutines'];
