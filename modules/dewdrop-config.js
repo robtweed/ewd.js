66c68d2fcf8a6aa1322fb320ff50204ee8444323
Rob Tweed <rtweed@mgateway.com>
2014-05-12 13:43:25 +0100

    Build 61.9: Modification to dewdrop runtime config file

--- modules/dewdrop-config.js
+++ modules/dewdrop-config.js
@@ -1,15 +1,2 @@
-var fs = require('fs');
-var exec = require('child_process').exec;
-var gtmdir = '/opt/lsb-gtm';
-var gtmver = '6.0-001_i686';
-
-if (!fs.existsSync('/usr/local/lib/libgtmshr.so')) {
-  var contents = '#!/usr/bin/env bash\nsudo -i\ncd /usr/local/lib\nln -s ' + gtmdir + '/' + gtmver + '/libgtmshr.so\nldconfig\nexit';
-  fs.writeFileSync('tmp.sh', contents, 'utf-8');
-  var ok = exec('tmp.sh', function() {
-    fs.unlinkSync('tmp.sh');
-  });
-};
-
 process.env['GTMCI'] = process.cwd() + '/node_modules/nodem/resources/calltab.ci';
 process.env['gtmroutines'] = process.cwd() + '/node_modules/nodem/src ' + process.env['gtmroutines'];
