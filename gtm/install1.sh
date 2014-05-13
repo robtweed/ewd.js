cdd5992a7251222d68c493b917a3d9b348847d00
Rob Tweed <rtweed@mgateway.com>
2014-05-13 13:48:44 +0100

    Build 61.11: Enhanced installer for standalone Ubuntu / GT.M

--- gtm/install1.sh
+++ gtm/install1.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+# Update first just to be sure
+
+sudo apt-get update
+
 # Install GT.M
 
 sudo apt-get install -y fis-gtm
@@ -10,6 +14,7 @@ echo -e 'H\n' | /usr/lib/fis-gtm/V6.0-003_x86_64/gtm -direct
 
 # Install NVM (Node.js Version Manager)
 
+sudo apt-get install -y curl
 curl https://raw.githubusercontent.com/creationix/nvm/v0.7.0/install.sh | sh
 
 # Now stop terminal process and restart, then run part 2 of installer
