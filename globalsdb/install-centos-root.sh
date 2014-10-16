c558fc4969830a53bb35a5daad42e4b5d1c00fdb
Rob Tweed <rtweed@mgateway.com>
2014-10-16 11:03:34 +0100

    Build 86.1: Added Centos root installer and startup file for GlobalsDB

--- globalsdb/install-centos-root.sh
+++ globalsdb/install-centos-root.sh
@@ -1,37 +1,43 @@
 #!/usr/bin/env bash
 
-# Install GlobalsDB, Node.js and EWD.js on Ubuntu System
+# Install GlobalsDB, Node.js and EWD.js on Centos 6.5 System
+#  Assumes you've logged in as root
+#  To get hold of and run this installer:
+#    cd /opt
+#    yum install -y subversion
+#    svn export https://github.com/robtweed/ewd.js/trunk/globalsdb globalsdb
+#    source globalsdb/install-centos-root.sh
 
 # Update first just to be sure
 
-sudo apt-get update
-sudo apt-get install -y wget gzip openssh-server curl
+yum update
+yum install -y wget gzip openssh-server curl
 
 # Install GlobalsDB
 
 # First increase shared memory quotas
 
-sudo sysctl -w kernel.shmall=536870912
-sudo sysctl -w kernel.shmmax=536870912
-sudo /bin/su -c "echo 'kernel.shmall=536870912' >> /etc/sysctl.conf"
-sudo /bin/su -c "echo 'kernel.shmmax=536870912' >> /etc/sysctl.conf"
+sysctl -w kernel.shmall=536870912
+sysctl -w kernel.shmmax=536870912
+echo 'kernel.shmall=536870912' >> /etc/sysctl.conf
+echo 'kernel.shmmax=536870912' >> /etc/sysctl.conf
 
-cd ~
+cd /opt
 wget http://globalsdb.org/sites/default/files/globals_2013.2.0.350.0_unix.tar.gz
 
 gzip -cd globals_2013.2.0.350.0_unix.tar.gz | tar -x
 rm globals_2013.2.0.350.0_unix.tar.gz
 cd kit_unix_globals
-mkdir ~/globalsdb
+mkdir /opt/globalsdb
 ISC_QUIET=yes
 export ISC_QUIET
-ISC_TGTDIR=~/globalsdb
+ISC_TGTDIR=/opt/globalsdb
 export ISC_TGTDIR
 ISC_PLATFORM=lnxsusex64
 export ISC_PLATFORM
 ./installGlobals
 
-cd ~
+cd /opt
 rm -rf kit_unix_globals
 
 # Install NVM
@@ -43,28 +49,29 @@ nvm install 0.10
 nvm use default
 echo 'nvm use default' >> ~/.profile
 
-# Now ready to install EWD.js:
+# Now ready to install EWD.js and Nodem:
 
-cd ~
+cd /opt
 mkdir ewdjs
 cd ewdjs
 npm install ewdjs
 
+
 # Now install/configure EWD.js
 
 cd node_modules/ewdjs
-node install silent ~/ewdjs
+node install silent /opt/ewdjs
 
 # Move the Node.js interface file into the correct place
 
-mv ~/globalsdb/bin/cache0100.node ~/ewdjs/node_modules/cache.node
+mv /opt/globalsdb/bin/cache0100.node /opt/ewdjs/node_modules/cache.node
 
-cd ~/ewdjs
+cd /opt/ewdjs
 
 # now ready to start EWD.js using:
 
-# cd ~/ewdjs
-# node ewdStart-globals
+# cd /opt/ewdjs
+# node ewdStart-gtm gtm-config
 
 
  
---
+++ startupExamples/ewdStart-globals-centos-root.js
@@ -0,0 +1,55 @@
+/*
+Example EWD.js Startup file for use with GlobalsDB running on Linux or OS X
+
+Notes:
+
+1) Change the database.path value as appropriate for your GlobalsDB installation
+
+2) IMPORTANT!: The cache.node interface module file MUST exist in the primary node_modules directory
+of your EWD.js configuration. 
+
+3) Comment out the cwd parameter if you want to use this startup file from the command line
+
+4) You may need to invoke this startup file as sudo if you have used Mike Clayton's Ubuntu installer for EWD.js
+
+*/
+
+var ewd = require('ewdjs');
+var config = {};
+if (process.argv[2]) config = require(process.argv[2]);
+
+var defaults = {
+  cwd: '/opt/ewdjs',
+  path: '/opt/globalsdb/mgr',
+  port: 8080,
+  poolsize: 2,
+  tracelevel: 3,
+  password: 'keepThisSecret!',
+  ssl: false
+};
+
+if (config.setParams) {
+  var overrides = config.setParams();
+  for (var name in overrides) {
+    defaults[name] = overrides[name];
+  }
+}
+
+params = {
+  cwd: defaults.cwd,
+  httpPort: defaults.port,
+  poolSize: defaults.poolsize,
+  database: {
+    type: 'globals',
+    path: defaults.path,
+  },
+  https: {
+    enabled: defaults.ssl
+  },
+  traceLevel: defaults.tracelevel,
+  management: {
+    password: defaults.password
+  }
+};
+
+ewd.start(params);
