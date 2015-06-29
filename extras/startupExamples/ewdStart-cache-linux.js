bcc88d8172d35103afac42d9299d51d27bb9e779
Rob Tweed <rtweed@mgateway.com>
2015-06-29 14:25:51 +0100

    Build 100.1: Added example startup file for Cache on Linux

---
+++ extras/startupExamples/ewdStart-cache-linux.js
@@ -0,0 +1,32 @@
+/*
+Example EWD.js Startup file for use with Cache on Linux
+
+Notes:
+
+1) Change the database.path value as appropriate for your Cache installation.  Also change the
+    password etc if required
+
+2) IMPORTANT!: The cache.node interface module file MUST exist in the primary node_modules directory
+of your EWD.js configuration
+
+*/
+
+var ewd = require('ewdjs');
+
+var params = {
+  poolSize: 2,
+  httpPort: 8080,
+  traceLevel: 3,
+  database: {
+    type: 'cache',
+    path:"/opt/cache/mgr",
+    username: "_SYSTEM",
+    password: "SYS",
+    namespace: "USER"
+  },
+  management: {
+    password: 'keepThisSecret!'
+  }
+};
+
+ewd.start(params);
